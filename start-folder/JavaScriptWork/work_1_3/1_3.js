4bc3ba0f6b4886cb8b952942776b995a64671887
RiLLiaN <srin5ame@gmail.com>
2025-06-04 16:20:14 +0900

    addText

--- start-folder/JavaScriptWork/work_1_3/1_3.js
+++ start-folder/JavaScriptWork/work_1_3/1_3.js
@@ -4,7 +4,7 @@ const addTax = total => total * 1.1;
 
 const total = getTotalPrice(1000, 2); // → 2000
 
-console.log(total);
+console.log(`税込金額は${total}円です`);
 
 const taxedTotal = addTax(total);     // → 2200
 
